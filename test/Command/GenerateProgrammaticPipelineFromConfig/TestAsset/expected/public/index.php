19098a904ba919a975adb5e29412851b7fb03584
Matthew Weier O'Phinney <matthew@zend.com>
2016-11-18 10:47:14 -0600

    Add missing test asset (git ignored by default)

---
+++ test/Command/GenerateProgrammaticPipelineFromConfig/TestAsset/expected/public/index.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(error_reporting() & ~E_USER_DEPRECATED);
+
+// Delegate static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server'
+    && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
+) {
+    return false;
+}
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+/** @var \Interop\Container\ContainerInterface $container */
+$container = require 'config/container.php';
+
+/** @var \Zend\Expressive\Application $app */
+$app = $container->get(\Zend\Expressive\Application::class);
+include 'config/pipeline.php';
+include 'config/routes.php';
+$app->run();
