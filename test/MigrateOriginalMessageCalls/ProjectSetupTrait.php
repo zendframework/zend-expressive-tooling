ab6c942ea2f79834dec9d818816a9a879380f7ac
webimpress <contact@webimpress.com>
2017-11-20 14:50:24 +0000

    Fixed migrate:original-messages tests
    Compare output from the converter with expected files contents

--- test/MigrateOriginalMessageCalls/ProjectSetupTrait.php
+++ test/MigrateOriginalMessageCalls/ProjectSetupTrait.php
@@ -8,6 +8,7 @@
 namespace ZendTest\Expressive\Tooling\MigrateOriginalMessageCalls;
 
 use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\Assert;
 use Prophecy\Argument;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
@@ -69,5 +70,25 @@ trait ProjectSetupTrait
             ->shouldBeCalled();
 
         return $console;
+    }
+
+    public function assertExpected(string $dir)
+    {
+        $base = $dir;
+        $rdi = new RecursiveDirectoryIterator($dir);
+        $rii = new RecursiveIteratorIterator($rdi);
+
+        /** @var SplFileInfo $file */
+        foreach ($rii as $file) {
+            if (! $this->isPhpFile($file)) {
+                continue;
+            }
+
+            $filename = $file->getPathname();
+            $content = file_get_contents($filename);
+            $name = strtr($filename, [$base . '/src' => __DIR__ . '/TestAsset/expected', DIRECTORY_SEPARATOR => '/']);
+
+            Assert::assertSame(file_get_contents($name), $content);
+        }
     }
 }
