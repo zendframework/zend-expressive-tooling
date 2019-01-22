b019df7d2ab808b391c653b4ac6d9c9a514ac327
Matthew Weier O'Phinney <matthew@zend.com>
2019-01-22 11:26:59 -0600

    qa: Import all traits, update copyrights
    Imports the TemplateResolutionTrait in classes that need it (instead of
    using global resolution).
    
    Updates copyright range in all changed files.

--- src/Module/Create.php
+++ src/Module/Create.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-tooling for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2017-2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-tooling/blob/master/LICENSE.md New BSD License
  */
 
@@ -9,9 +9,11 @@ declare(strict_types=1);
 
 namespace Zend\Expressive\Tooling\Module;
 
+use Zend\Expressive\Tooling\TemplateResolutionTrait;
+
 class Create
 {
-    use \Zend\Expressive\Tooling\TemplateResolutionTrait;
+    use TemplateResolutionTrait;
 
     public const TEMPLATE_CONFIG_PROVIDER = <<< 'EOT'
 <?php
--- test/Module/CreateTest.php
+++ test/Module/CreateTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-tooling for the canonical source repository
- * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2017-2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-tooling/blob/master/LICENSE.md New BSD License
  */
 
